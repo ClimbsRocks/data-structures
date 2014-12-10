b062c526251fc2806422ed2a6f6082740d3cbbec
ClimbsRocks <ClimbsRocks@users.noreply.github.com>
2014-12-10 12:14:11 -0800

    got the bones of functional shared stack, but not working

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
+  var storage = {};
+  var counter = 0;
+
+  this.push = stackMethods.push;
+  this.pop = stackMethods.pop;
+  this.size = stackMethods.size;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[counter] = value;
+  this.counter++;
+};
+
+stackMethods.pop = function() {
+  if(this.counter > 0) {
+    this.counter--;
+    var returnVal = this.storage[counter];
+    delete this.storage[counter];
+    return returnVal;
+  }
+};
+
+stackMethods.size = function() {
+  return counter;
+}
 
