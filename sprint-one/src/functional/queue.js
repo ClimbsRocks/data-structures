dc73279e5679282276cd211a1c262a37000a85da
climbsrocks <climbsbytes@gmail.com>
2014-12-10 16:24:08 -0800

    queue moving window style

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var minPosition = 0;
+  var maxPosition = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[maxPosition] = value;
+    maxPosition++;
   };
 
   someInstance.dequeue = function(){
+    if(maxPosition > minPosition){
+      var returnVal = storage[minPosition];
+      delete storage[minPosition];
+      minPosition++;
+      return returnVal;
+    }
   };
 
   someInstance.size = function(){
+    return maxPosition - minPosition;
   };
 
   return someInstance;
