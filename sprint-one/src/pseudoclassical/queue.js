5c826849b91380f260c4f386156786f36ff6896a
climbsrocks <climbsbytes@gmail.com>
2014-12-11 15:38:38 -0800

    pseudoclassical queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,26 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
+  this.storage = {};
+  this.minCounter = 0;
+  this.maxCounter = 0;
+
 };
 
+Queue.prototype.enqueue = function(value){
+  this.storage[this.maxCounter] = value;
+  this.maxCounter++;
+}
+
+Queue.prototype.dequeue = function(){
+  if(this.maxCounter > this.minCounter){
+    var tempVal = this.storage[this.minCounter];
+    delete this.storage[this.minCounter];
+    this.minCounter++;
+    return tempVal;
+  }
+}
+
+Queue.prototype.size = function(){
+  return this.maxCounter - this.minCounter;
+}
 
