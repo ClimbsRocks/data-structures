eec4175b90bd4c6adfc9cd40de2ee3487493d9f3
climbsrocks <climbsbytes@gmail.com>
2014-12-11 11:39:54 -0800

    Prototypal queue completed

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.minCounter = 0;
+  someInstance.maxCounter = 0;
+
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[this.maxCounter] = value;
+  this.maxCounter++;
+}
 
+queueMethods.dequeue = function() {
+  if(this.maxCounter > this.minCounter) {
+    var tempReturn = this.storage[this.minCounter];
+    delete this.storage[this.minCounter];
+    this.minCounter++;
+    return tempReturn;
+  }
+}
+
+queueMethods.size = function() {
+  return this.maxCounter - this.minCounter;
+}
