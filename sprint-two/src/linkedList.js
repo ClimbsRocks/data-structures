03ebd53f000053f99975c867534255d514a07d61
ClimbsRocks <ClimbsRocks@users.noreply.github.com>
2014-12-11 21:48:44 -0800

    finished linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,49 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    // debugger
+    var nodeVal = Node(value);
+    if(list.head === null) {
+      list.head = nodeVal;
+      list.tail = nodeVal;
+    } else if(list.tail === list.head) {
+      list.head.next = nodeVal;
+      list.tail = nodeVal;
+    } else {
+      list.tail.next = nodeVal;
+      list.tail = nodeVal;
+    }
   };
 
   list.removeHead = function(){
+    if(list.head === list.tail) {
+      var tempReturn = list.head.value;
+      var tempHead = list.head.next;
+      delete list.head;
+      list.head = tempHead;
+      list.tail = null;
+      return tempReturn;
+    } else if(list.head !== null) {
+      var tempReturn = list.head.value;
+      var tempHead = list.head.next;
+      delete list.head;
+      list.head = tempHead;
+      return tempReturn;
+    }
   };
 
   list.contains = function(target){
+    var rContains = function(inNode) {
+      if(inNode.value === target) {
+        return true;
+      } else if(inNode.next === null) {
+        return false;
+      } else {
+        return rContains(inNode.next);
+      }
+    }
+
+    return rContains(list.head);
   };
 
   return list;
