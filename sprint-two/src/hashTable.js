ee1f3b091544f311b73415700d5f459fd820d4f8
climbsrocks <climbsbytes@gmail.com>
2014-12-12 23:42:37 -0800

    adding resizing to hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,7 +1,17 @@
-var HashTable = function(){
+var HashTable = function(inSize){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._thingsStored = 0;
 };
+
+HashTable.prototype.resize = function() {
+  var nodes = this.getAllNodes();
+  this._limit = this._limit*2;
+  this._storage = LimitedArray(this._limit);
+  for(var i = 0; i < nodes.length; i++) {
+    this.insert(nodes[i][0], nodes[i][1]);
+  }
+}
 
 HashTable.prototype.insert = function(k, v){
   var tuple = [k, v];
@@ -15,7 +25,28 @@ HashTable.prototype.insert = function(k, v){
     alreadyStored = [tuple];
   }
   this._storage.set(i, alreadyStored);
+  this._thingsStored++;
+  if(this._thingsStored > .75*this._limit) {
+    this.resize();
+  }
 };
+
+HashTable.prototype.getAllNodes = function() {
+  debugger;
+  var nodes = [];
+  for(var i = 0; i < this._limit; i++) {
+    if(this._storage[i].length > 1) {
+      for(var j = 0; j < this._storage[i].length; j++) {
+        nodes.push(this._storage[i][j]);
+      }
+    } else {
+      nodes.push(this._storage[i]);
+    }
+  }
+  return nodes;
+}
+
+
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
