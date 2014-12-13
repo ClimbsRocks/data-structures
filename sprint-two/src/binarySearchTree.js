713d72a2885bb5ccf896c665e9595f143f528630
climbsrocks <climbsbytes@gmail.com>
2014-12-13 12:19:04 -0800

    first attempt at binary search tree, wrong instantiation pattern for test

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,37 @@
 var BinarySearchTree = function(value){
+  var node = {};
+  var value = value;
+  var parent = null;
+  if(this !== null) {
+    parent = this;
+  }
+  var left = null;
+  var right = null;
+
+  var insert = function(value) = {
+    var newTree = new BinarySearchTree(value);
+
+    //can likely be refactored using ternary operators
+    var findPosition = function(node) {
+
+      if(value < this.value) {
+        this.left === null ? this.left = newTree : findPosition(this.left);
+      } else if(value > this.value) {
+        this.right === null ? this.right = newTree : findPosition(this.right);
+      }
+    }
+    findPosition(this);
+  }
+
+  var
+
+  var
+  return node;
+
+
 };
+
+
 
 
 /*
