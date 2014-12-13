473950d253c4ce129a0b81d21dc230ead68e9fa6
climbsrocks <climbsbytes@gmail.com>
2014-12-13 12:30:41 -0800

    different instantiation pattern on BST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,35 +1,68 @@
-var BinarySearchTree = function(value){
+var BinarySearchTree = function(value) {
   var node = {};
-  var value = value;
-  var parent = null;
+  node.value = value;
+  node.right = null;
+  node.left = null;
+  node.parent = null;
   if(this !== null) {
-    parent = this;
+    node.parent = this;
   }
-  var left = null;
-  var right = null;
-
-  var insert = function(value) = {
-    var newTree = new BinarySearchTree(value);
-
-    //can likely be refactored using ternary operators
+  node.insert = function(value) {
+    var newTree = BinarySearchTree(value);
     var findPosition = function(node) {
-
-      if(value < this.value) {
-        this.left === null ? this.left = newTree : findPosition(this.left);
-      } else if(value > this.value) {
-        this.right === null ? this.right = newTree : findPosition(this.right);
+      if(value < node.value) {
+        node.left === null ? node.left = newTree : findPosition(node.left);
+      } else if (value > node.value) {
+        node.right === null ? node.right = newTree : findPosition(node.right);
       }
     }
-    findPosition(this);
+    findPosition(node);
   }
 
-  var
+  node.contains = function() {
 
-  var
+  }
+
+  node.depthFirstLog = function(callback) {
+
+  }
+
   return node;
 
+}
 
-};
+// var BinarySearchTree = function(value){
+//   var node = {};
+//   var value = value;
+//   var parent = null;
+//   if(this !== null) {
+//     parent = this;
+//   }
+//   var left = null;
+//   var right = null;
+
+//   var insert = function(value) = {
+//     var newTree = new BinarySearchTree(value);
+
+//     //can likely be refactored using ternary operators
+//     var findPosition = function(node) {
+
+//       if(value < this.value) {
+//         this.left === null ? this.left = newTree : findPosition(this.left);
+//       } else if(value > this.value) {
+//         this.right === null ? this.right = newTree : findPosition(this.right);
+//       }
+//     }
+//     findPosition(this);
+//   }
+
+//   var
+
+//   var
+//   return node;
+
+
+// };
 
 
 
