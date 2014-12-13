4e2a3e90087aeb83b706c2db06ee881ac31d012c
climbsrocks <climbsbytes@gmail.com>
2014-12-12 16:11:14 -0800

    added a bit more to graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -17,6 +17,17 @@ Graph.prototype.contains = function(node){
 };
 
 Graph.prototype.removeNode = function(node){
+  for(var i = 0; i < this.storage.length; i++){
+    //implement nested for loop to loop through the edges array on each node
+    //if that edge is the one we're trying to remove, remove it from that node's edges array as well.
+  }
+
+
+  for(var i = 0; i < this.storage.length; i++){
+    if(this.storage[i].value === node){
+      this.storage.splice(i, 1);
+    }
+  }
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
@@ -26,18 +37,25 @@ Graph.prototype.addEdge = function(fromNode, toNode){
   var tempFrom;
   var tempTo;
   for (var i = 0; i < this.storage.length; i++) {
-    if(this.storage[i] === fromNode.value) {
+    if(this.storage[i].value === fromNode) {
       tempFrom = this.storage[i];
-    } else if(this.storage[i] === toNode.value) {
+    } else if(this.storage[i].value === toNode) {
       tempTo = this.storage[i];
     }
   }
+  tempFrom.edges.push(tempTo);
+  tempTo.edges.push(tempFrom);
+
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
 };
 
 Graph.prototype.forEachNode = function(cb){
+  for(var i = 0; i < this.storage.length; i++) {
+    //this may need to only be called on the node itself, not the node's value
+    cb(this.storage[i].value);
+  }
 };
 
 /*
