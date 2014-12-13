9f1cb42e38415f89e2f4c384be4e5ffc1d902410
climbsrocks <climbsbytes@gmail.com>
2014-12-12 16:38:46 -0800

    started hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,16 +4,32 @@ var HashTable = function(){
 };
 
 HashTable.prototype.insert = function(k, v){
+
+  //make each stored value a tuple of key value pairs
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var alreadyStored = this._storage.get(i);
+  if(alreadyStored !== undefined) {
+    if(Array.isArray(alreadyStored)) {
+      alreadyStored.push(v);
+    } else {
+      var tempArr = [alreadyStored];
+      tempArr.push(v);
+      this._storage.set(i, tempArr);
+    }
+  } else{
+    this._storage.set(i,v);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  return this._storage.get(i);
 };
 
 HashTable.prototype.remove = function(k){
-
+  debugger;
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.splice(i,1);
 };
 
 
@@ -21,3 +37,4 @@ HashTable.prototype.remove = function(k){
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
