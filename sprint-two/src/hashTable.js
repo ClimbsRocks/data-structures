38fdcc02613518db2955b1e31f02d42dacf354c7
climbsrocks <climbsbytes@gmail.com>
2014-12-12 20:57:48 -0800

    cleaned up hash slightly

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,25 +4,6 @@ var HashTable = function(){
 };
 
 HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
-};
-
-HashTable.prototype.retrieve = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
-
-};
-
-HashTable.prototype.remove = function(k){
-
-};
-
-//Preston's original code below:
-var HashTable = function(){
-  this._limit = 8;
-  this._storage = LimitedArray(this._limit);
-};
-
-HashTable.prototype.insert = function(k, v){
   var tuple = [k, v];
 
   //make each stored value a tuple of key value pairs
